a288ef104ee584015954aad93734e2a129bcf084
Gunasekar M <81707727+GUNASEKARsekar@users.noreply.github.com>
2022-06-29 10:37:58 +0530

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,30 @@
+<div class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="indexx.php">Email Functions</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li class="nav-item"><a class="nav-link" aria-current="page" href="indexx.php">Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="register.php">Create Account</a></li>
+                                <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="indexx.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Emails</a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a class="dropdown-item" href="index3.php">SMS</a></li>
+                                        <li><a class="dropdown-item" href="index2.php">Bulk Email</a></li>
+                                    </ul>   
+                                </li>
+                                <li class="nav-item"><a class ="nav-link" href="index.php">Logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
